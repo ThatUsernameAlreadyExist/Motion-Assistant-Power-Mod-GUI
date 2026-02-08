796664884ff6ec12b7d103759e6fa711e6e3628c
Alexander <ThatUsernameAlreadyExist@cock.li>
2026-02-08 16:04:21 +0200

    CPU page - remove United TDP/FPS toggles. Adding keyboard slider interaction (WIP).

--- Views/Pages/CPUPage.axaml.cs
+++ Views/Pages/CPUPage.axaml.cs
@@ -6,6 +6,7 @@ using PmGui.ViewModels;
 using PmGui.Managers;
 using System;
 using System.Reflection;
+using Avalonia.Threading;
 
 namespace PmGui.Views.Pages
 {
@@ -16,45 +17,82 @@ namespace PmGui.Views.Pages
             InitializeComponent();
         }
 
+        private IDisposable _debounceTimerPowerline;
+        private IDisposable _debounceTimerBattery;
+
+        private void SendPowerlineTdpValue(int value)
+        {
+            if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                var mainWindow = desktop.MainWindow;
+                if (mainWindow?.DataContext is MainWindowViewModel mainWindowVm)
+                {
+                    var cpuVm = mainWindowVm.CPUPageViewModel;
+                    if (cpuVm != null)
+                    {
+                        GlobalAppManager.Instance.SendCmdPowerLineTdpValue(value);
+                    }
+                }
+            }
+        }
+
+        private void SendBatteryTdpValue(int value)
+        {
+            if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                var mainWindow = desktop.MainWindow;
+                if (mainWindow?.DataContext is MainWindowViewModel mainWindowVm)
+                {
+                    var cpuVm = mainWindowVm.CPUPageViewModel;
+                    if (cpuVm != null)
+                    {
+                        GlobalAppManager.Instance.SendCmdBatteryTdpValue(value);
+                    }
+                }
+            }
+        }
+
         private void OnPowerlineTdpDragCompleted(object sender, Avalonia.Input.PointerCaptureLostEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine($"Event fired: {MethodBase.GetCurrentMethod()?.Name}");
 
             if (sender is Slider slider)
             {
-                if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                var currentValue = (int)slider.Value;
+
+                // Cancel any existing timer
+                _debounceTimerPowerline?.Dispose();
+                _debounceTimerPowerline = null;
+
+
+                // For ValueChanged events, use 1-second debounce with captured value
+                _debounceTimerPowerline = DispatcherTimer.RunOnce(() =>
                 {
-                    var mainWindow = desktop.MainWindow;
-                    if (mainWindow?.DataContext is MainWindowViewModel mainWindowVm)
-                    {
-                        var cpuVm = mainWindowVm.CPUPageViewModel;
-                        if (cpuVm != null)
-                        {
-                            GlobalAppManager.Instance.SendCmdPowerLineTdpValue((int)slider.Value);
-                        }
-                    }
-                }
+                    SendPowerlineTdpValue(currentValue);
+                    _debounceTimerPowerline = null;
+                }, TimeSpan.FromSeconds(1));
             }
         }
 
         private void OnBatteryTdpDragCompleted(object sender, Avalonia.Input.PointerCaptureLostEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine($"Event fired: {MethodBase.GetCurrentMethod()?.Name}");
-            
+
             if (sender is Slider slider)
             {
-                if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+                var currentValue = (int)slider.Value;
+
+                // Cancel any existing timer
+                _debounceTimerBattery?.Dispose();
+                _debounceTimerBattery = null;
+
+
+                // For ValueChanged events, use 1-second debounce with captured value
+                _debounceTimerBattery = DispatcherTimer.RunOnce(() =>
                 {
-                    var mainWindow = desktop.MainWindow;
-                    if (mainWindow?.DataContext is MainWindowViewModel mainWindowVm)
-                    {
-                        var cpuVm = mainWindowVm.CPUPageViewModel;
-                        if (cpuVm != null)
-                        {
-                            GlobalAppManager.Instance.SendCmdBatteryTdpValue((int)slider.Value);
-                        }
-                    }
-                }
+                    SendBatteryTdpValue(currentValue);
+                    _debounceTimerBattery = null;
+                }, TimeSpan.FromSeconds(1));
             }
         }
     }
